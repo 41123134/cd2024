d3d2186e0b9f28b966df23eea6b8fe9f7b402ae6
41123134 <115727821+41123134@users.noreply.github.com>
2024-02-26 16:27:41 +0000

    更新個人網站部落格。

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'KMOL'
-SITENAME = '網際內容管理系統'
+AUTHOR = '41123134'
+SITENAME = '協同產品設計實習41123134'
 # 不要用文章所在目錄作為類別
 USE_FOLDER_AS_CATEGORY = False
 
